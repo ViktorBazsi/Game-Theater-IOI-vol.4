83b476a91831212e9b9498a676dcf9e0395958ae
ViktorBazsi <viktor.balazs.endre@gmail.com>
2025-02-24 10:20:41 +0100

    DONE - basic CRUD-s for User

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { errorHandler } from "./middleware/errorHandlingMiddleware.js";
 
+import userRoutes from "./routes/user.routes.js";
+
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/user", userRoutes);
 
 app.use(errorHandler);
 
---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import userController from "../controllers/user.controller.js";
+// import authMiddleware from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+// POST
+router.post("/", userController.create);
+// GET
+router.get("/", userController.list);
+router.get("/:id", userController.getById);
+// PUT
+router.put("/:id", userController.update);
+// DELETE
+router.delete("/:id", userController.destroy);
+
+export default router;
